2fdb9e4bb965c727e7b8e328dd77a311f92b9d74
TalasaDev <119393713+TalasaDev@users.noreply.github.com>
2023-01-27 15:22:25 +1000

    Update script.js
    Dimmer toggle effect added

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ const navbar = document.querySelector(".navbar");
 const burger = document.querySelector(".burger-menu");
 const menuIcon = document.querySelector(".menu-icon");
 const closeIcon = document.querySelector(".close-icon");
-const shadow = document.querySelector(".shadow");
+const dimmer = document.querySelector(".dimmer");
 
 function toggle() {
     if (navbar.classList.contains ("showNavbar")) {
         navbar.classList.remove("showNavbar");
         closeIcon.style.display = "none";
         menuIcon.style.display = "block";
-        
+        dimmer.style.display = "none";              
 
     } else {
         navbar.classList.add("showNavbar");
         closeIcon.style.display = "block";
         menuIcon.style.display = "none";
-        
+        dimmer.style.display = "block";
     }
 }
 
